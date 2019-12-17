7b65ab7131e5afbed201394ebc1324e8d82b185a
olivermanzi <olivermanzi.dev@gmail.com>
2019-12-17 22:42:52 +0100

    resolves #16 by improving input fields for small screens

--- src/components/Clutter.vue
+++ src/components/Clutter.vue
@@ -14,44 +14,70 @@
       <!-- word -->
       <b-row id="word">
         <b-col>
-          <b-row id="clutteredWord">
-            <b-col>{{clutteredCountry}}</b-col>
-          </b-row>
-          <b-row id="hint" v-if="hint">
-            <b-col>
-              <b-col>{{hintText}}</b-col>
-            </b-col>
-          </b-row>
-          <b-row id="input" v-if="gameStarted">
-            <b-col>
-              <b-input-group>
-                <b-form-input
-                  size="lg"
-                  :disabled="gameFinished"
-                  v-model="userInput"
-                  v-on:keyup.enter="checkAnswer"
-                ></b-form-input>
-                <b-input-group-append>
-                  <b-button
-                    size="lg"
-                    text="Button"
-                    variant="success"
-                    :disabled="gameFinished"
-                    @click="checkAnswer"
-                  >Take a Guess</b-button>
-                </b-input-group-append>
-                <b-input-group-append>
-                  <b-button
-                    size="lg"
-                    text="Button"
-                    variant="warning"
-                    :disabled="gameFinished"
-                    @click="skipAnswer"
-                  >Skip</b-button>
-                </b-input-group-append>
-              </b-input-group>
-            </b-col>
-          </b-row>
+          <div id="output">
+            <b-row id="clutter">
+              <b-col>{{clutteredCountry}}</b-col>
+            </b-row>
+            <b-row id="hint" v-if="hint">
+              <b-col>
+                <b-col>{{hintText}}</b-col>
+              </b-col>
+            </b-row>
+          </div>
+          <div id="input">
+            <b-row v-if="gameStarted">
+              <b-col>
+                <b-form-group>
+                  <b-input-group>
+                    <b-form-input
+                      :size="this.getScreenSize"
+                      :disabled="gameFinished"
+                      v-model="userInput"
+                      v-on:keyup.enter="checkAnswer"
+                    ></b-form-input>
+                    <div v-if="this.getScreenSize == 'lg'">
+                      <b-input-group-append>
+                        <b-button
+                          :size="this.getScreenSize"
+                          text="Button"
+                          variant="success"
+                          :disabled="gameFinished"
+                          @click="checkAnswer"
+                        >Take a Guess</b-button>
+                      </b-input-group-append>
+                      <b-input-group-append>
+                        <b-button
+                          :size="this.getScreenSize"
+                          text="Button"
+                          variant="warning"
+                          :disabled="gameFinished"
+                          @click="skipAnswer"
+                        >Skip</b-button>
+                      </b-input-group-append>
+                    </div>
+                  </b-input-group>
+                  <b-form-group v-if="this.getScreenSize != 'lg'" label-cols-sm="12">
+                    <b-button-group>
+                      <b-button
+                        :size="this.getScreenSize"
+                        text="Button"
+                        variant="success"
+                        :disabled="gameFinished"
+                        @click="checkAnswer"
+                      >Guess</b-button>
+                      <b-button
+                        :size="this.getScreenSize"
+                        text="Button"
+                        variant="warning"
+                        :disabled="gameFinished"
+                        @click="skipAnswer"
+                      >Skip</b-button>
+                    </b-button-group>
+                  </b-form-group>
+                </b-form-group>
+              </b-col>
+            </b-row>
+          </div>
         </b-col>
       </b-row>
       <!-- buttons -->
@@ -59,12 +85,27 @@
         <b-col>
           <b-button-toolbar>
             <b-button-group>
-              <b-button variant="primary" :disabled="gameStarted" @click="startGame" size="lg">Start</b-button>
-              <b-button variant="warning" v-if="gameStarted" @click="giveHint" size="lg">
+              <b-button
+                variant="primary"
+                :disabled="gameStarted"
+                @click="startGame"
+                :size="this.getScreenSize"
+              >Start</b-button>
+              <b-button
+                variant="warning"
+                v-if="gameStarted"
+                @click="giveHint"
+                :size="this.getScreenSize"
+              >
                 Hint
                 <b-badge variant="light">{{hintsLeft}}</b-badge>
               </b-button>
-              <b-button variant="danger" v-if="gameStarted" @click="restart" size="lg">Restart</b-button>
+              <b-button
+                variant="danger"
+                v-if="gameStarted"
+                @click="restart"
+                :size="this.getScreenSize"
+              >Restart</b-button>
             </b-button-group>
           </b-button-toolbar>
         </b-col>
@@ -120,20 +161,63 @@ export default {
       //game rules
       timer: 60,
       gameTime: 10,
-      maxMistakes: 3
+      maxMistakes: 3,
+      //other
+      screenSize: { width: screen.width, height: screen.height }
     };
   },
   computed: {
     showTime: function() {
       return this.timer;
+    },
+    getScreenSize: function() {
+      var size = this.screenSize.width;
+      var res = "";
+      if (size <= 350) {
+        res = "sm";
+      } else if (350 < size && size <= 450) {
+        res = "Default";
+      } else {
+        res = "lg";
+      }
+      console.log(res);
+      return res;
     }
   },
   methods: {
+    //rules
     startGame: function() {
       this.gameStarted = true;
       this.fetchRandomCountry();
       this.startTimer();
     },
+    startTimer: function() {
+      var component = this;
+      this.timerObject = setInterval(function() {
+        component.timer--;
+        if (component.timer == 0) {
+          clearInterval(component.timerObject);
+          component.totalIncorrectWords.push(component.country);
+          component.gameFinished = true;
+        }
+      }, 1000);
+    },
+    fetchRandomCountry: function() {
+      var component = this;
+      CountryController.fetchCountries().then(function(list) {
+        var country = list[ClutterTool.randomNumber(0, list.length)].name;
+        var clutteredCountry = ClutterTool.clutter(country);
+        component.country = country;
+        component.clutteredCountry = clutteredCountry;
+        component.totalWords.push(country);
+      });
+    },
+    decreaseScore: function() {
+      if (this.score < 0) {
+        this.score--;
+      }
+    },
+    //option
     restart: function() {
       //country
       this.country = "clutter";
@@ -171,11 +255,6 @@ export default {
       this.decreaseScore();
       this.fetchRandomCountry();
     },
-    decreaseScore: function() {
-      if (this.score < 0) {
-        this.score--;
-      }
-    },
     giveHint: function() {
       if (this.hint == false) {
         if (this.hintsLeft == 0) {
@@ -204,33 +283,21 @@ export default {
         this.hint = false;
       }
     },
-    startTimer: function() {
-      var component = this;
-      this.timerObject = setInterval(function() {
-        component.timer--;
-        if (component.timer == 0) {
-          clearInterval(component.timerObject);
-          component.totalIncorrectWords.push(component.country);
-          component.gameFinished = true;
-        }
-      }, 1000);
-    },
-    fetchRandomCountry: function() {
-      var component = this;
-      CountryController.fetchCountries().then(function(list) {
-        var country = list[ClutterTool.randomNumber(0, list.length)].name;
-        var clutteredCountry = ClutterTool.clutter(country);
-        component.country = country;
-        component.clutteredCountry = clutteredCountry;
-        component.totalWords.push(country);
-      });
+    //other
+    fetchScreenSize: function() {
+      this.screenSize = { width: screen.width, height: screen.height };
+      console.log(this.screenSize.width);
     }
+  },
+  beforeMount: function() {
+    this.fetchScreenSize();
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+/* scoreboard */
 #scoreboard {
   margin-top: 10vh;
   padding: 2vh 2vw;
@@ -247,6 +314,7 @@ export default {
   text-align: end;
 }
 
+/* word */
 #word {
   margin-top: 10vh;
   padding: 2vh 2vw;
@@ -254,6 +322,14 @@ export default {
   font-size: 5em;
 }
 
+#output {
+  margin: 2.5vh 0vw;
+  padding: 1vh 1.5vw;
+  border-radius: 25px;
+  background-color: rgba(0, 0, 0, 0.247);
+}
+
+/* options */
 #buttons {
   margin-top: 10vh;
   padding: 2vh 2vw;
@@ -272,6 +348,22 @@ export default {
 
   #word {
     font-size: 2.5em;
+  }
+}
+
+@media only screen and (max-width: 350) {
+  #timer {
+    font-size: 1em;
+    font-weight: bold;
+  }
+
+  #score {
+    font-size: 1em;
+    font-weight: bold;
+  }
+
+  #word {
+    font-size: 1.5em;
   }
 }
 </style>
