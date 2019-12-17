c03eb85bfd24c7fa9ab547e468b2341907e45655
olivermanzi <olivermanzi.dev@gmail.com>
2019-12-17 23:23:28 +0100

    fixes styling that was causing overflow at the bottom of screen when playing game

--- src/components/Clutter.vue
+++ src/components/Clutter.vue
@@ -298,7 +298,7 @@ export default {
 /* scoreboard */
 #scoreboard {
   margin-top: 10vh;
-  padding: 2vh 2vw;
+  padding: 1vh 1vw;
 }
 
 #timer {
@@ -315,7 +315,7 @@ export default {
 /* word */
 #word {
   margin-top: 10vh;
-  padding: 2vh 2vw;
+  padding: 1vh 1vw;
 
   font-size: 5em;
 }
@@ -330,7 +330,7 @@ export default {
 /* options */
 #buttons {
   margin-top: 10vh;
-  padding: 2vh 2vw;
+  padding: 1vh 1vw;
 }
 
 @media only screen and (max-width: 600px) {
