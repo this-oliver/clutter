60531735f63a1c787dd9cea153e70a047fb438e0
olivermanzi <olivermanzi.dev@gmail.com>
2019-12-24 00:29:53 +0100

    adds table with highscore

--- src/controllers/Country.js
+++ src/controllers/Country.js
@@ -1,5 +1,5 @@
 /* jshint esversion: 2015 */
-var axios = require("axios")
+var axios = require("axios");
 
 var countryUrl = "https://restcountries.eu/rest/v2/all";
 var regionBaseUrl = "https://restcountries.eu/rest/v2/region/";
@@ -7,62 +7,62 @@ var regionBaseUrl = "https://restcountries.eu/rest/v2/region/";
 /**
  * @returns list of all countries
  */
-const fetchAllCountries = function () {
-  return axios.get(countryUrl).then(function (response) {
+const fetchAllCountries = function() {
+  return axios.get(countryUrl).then(function(response) {
     return response.data;
   });
-}
+};
 
 /**
  * @returns list of all eurpean countries
  */
-const fetchEuropeanCountries = function () {
-  return axios.get(regionBaseUrl + "europe").then(function (response) {
+const fetchEuropeanCountries = function() {
+  return axios.get(regionBaseUrl + "europe").then(function(response) {
     return response.data;
   });
-}
+};
 
 /**
  * @returns list of all african countries
  */
-const fetchAfricanCountries = function () {
-  return axios.get(regionBaseUrl + "africa").then(function (response) {
+const fetchAfricanCountries = function() {
+  return axios.get(regionBaseUrl + "africa").then(function(response) {
     return response.data;
   });
-}
+};
 
 /**
  * @returns list of all asian countries
  */
-const fetchAsianCountries = function () {
-  return axios.get(regionBaseUrl + "asia").then(function (response) {
+const fetchAsianCountries = function() {
+  return axios.get(regionBaseUrl + "asia").then(function(response) {
     return response.data;
   });
-}
+};
 
 /**
  * @returns list of all americas countries
  */
-const fetchAmericasCountries = function () {
-  return axios.get(regionBaseUrl + "americas").then(function (response) {
+const fetchAmericasCountries = function() {
+  return axios.get(regionBaseUrl + "americas").then(function(response) {
     return response.data;
   });
-}
+};
 
 /**
  * @returns list of all oceania countries
  */
-const fetchOceaniaCountries = function () {
-  return axios.get(regionBaseUrl + "oceania").then(function (response) {
+const fetchOceaniaCountries = function() {
+  return axios.get(regionBaseUrl + "oceania").then(function(response) {
     return response.data;
   });
-}
+};
 
 /**
- * 
+ *
  * @param {String} region Region of world. If empty, the method returns all countries
  */
-const fetchCountries = function (region) {
+const fetchCountries = function(region) {
   switch (region) {
     case "africa":
       return fetchAfricanCountries();
@@ -77,7 +77,7 @@ const fetchCountries = function (region) {
     default:
       return fetchAllCountries();
   }
-}
+};
 
 export default {
   fetchCountries
--- src/controllers/Users.js
+++ src/controllers/Users.js
@@ -1,2 +1,19 @@
 /* jshint esversion: 2015 */
-var admin = require("./../../config/dbConfig");
+var axios = require("axios");
+var highscoreUrl =
+  process.env.NODE_ENV == "production"
+    ? "https://dashboard.heroku.com/apps/clutter-api/api/highscores/"
+    : "http://localhost:8081";
+
+/**
+ * @returns list of all countries
+ */
+const fetchHighScores = function() {
+  return axios.get(highscoreUrl).then(function(response) {
+    return response.data;
+  });
+};
+
+export default {
+  fetchHighScores
+};
